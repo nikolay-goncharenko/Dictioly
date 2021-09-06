c7b8c2ac12aade6e913c759775a5d82757f7028e
Nikolay Goncharenko <hellkennedy@gmail.com>
2021-09-06 20:12:05 +0300

    Сделал не возможным создавать списки с пустыми именами, тоже самое с редактированием имен списков и так же нельзя добавить пустые строки внутри списков.

---
+++ ViewControllers/EditModalViewController.swift
@@ -0,0 +1,99 @@
+//
+//  EditModalViewController.swift
+//  Dictioly
+//
+//  Created by Nikolay Goncharenko on 03.06.2021.
+//
+
+import UIKit
+
+class EditModalViewController: UIViewController {
+
+	@IBOutlet weak var modalWindow: UIView!
+	@IBOutlet weak var modalHeadingLabel: UILabel!
+	@IBOutlet weak var modalTextField: UITextField!
+	@IBOutlet weak var modalEditButton: UIButton!
+	@IBOutlet weak var modalCloseButton: UIButton!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+//		view.backgroundColor = UIColor.gray.withAlphaComponent(0.7)
+//		modalWindow.backgroundColor = .white
+		modalWindow.layer.cornerRadius = 8
+		modalTextField.layer.cornerRadius = 8
+		modalEditButton.layer.cornerRadius = 8
+		modalCloseButton.layer.cornerRadius = 8
+		
+		modalTextField.delegate = self
+		modalTextField.becomeFirstResponder()
+		modalTextField.clearButtonMode = .whileEditing
+		modalTextField.layer.borderWidth = 0.1
+		modalTextField.layer.cornerRadius = 8
+		modalTextField.layer.masksToBounds = true
+		
+		modalEditButton.isEnabled = false
+		modalEditButton.backgroundColor = #colorLiteral(red: 0.9490322471, green: 0.9485961795, blue: 0.9704589248, alpha: 1)
+		modalEditButton.setTitleColor(#colorLiteral(red: 1, green: 0.2705882353, blue: 0.2274509804, alpha: 1), for: .disabled)
+		modalTextField.addTarget(self, action: #selector(blockingModalEditButton), for: .editingChanged)
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+	}
+	
+	// MARK: - Actions of ModalViewContoller
+	
+	@IBAction func editCurrentElement(_ sender: UIButton) {
+		if modalTextField.text?.isEmpty == false {
+			changeItem(at: saveIndex(at: indexOfCell), newNameItem: modalTextField.text!)
+		}
+		dismiss(animated: true, completion: nil)
+	}
+	
+	@objc func blockingModalEditButton() {
+		if modalTextField.text!.isEmpty == true {
+			modalEditButton.isEnabled = false
+			modalEditButton.backgroundColor = #colorLiteral(red: 0.9490322471, green: 0.9485961795, blue: 0.9704589248, alpha: 1)
+			modalEditButton.setTitleColor(#colorLiteral(red: 1, green: 0.2705882353, blue: 0.2274509804, alpha: 1), for: .disabled)
+		} else {
+			modalEditButton.isEnabled = true
+			modalEditButton.backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.8431372549, blue: 0.2941176471, alpha: 1)
+			modalEditButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+		}
+	}
+	
+	@IBAction func closeCurrentView(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+	
+
+	/*
+	// MARK: - Navigation
+
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		// Get the new view controller using segue.destination.
+		// Pass the selected object to the new view controller.
+	}
+	*/
+
+}
+
+extension EditModalViewController: UITextFieldDelegate {
+
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		modalTextField.resignFirstResponder()
+	}
+
+	@objc func keyboardWillShow(_ notification: Notification) {
+		let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+		if modalWindow.frame.maxY != keyboardSize!.origin.y && modalTextField.becomeFirstResponder() {
+			modalWindow.frame.origin.y -= keyboardSize!.height / 4
+		}
+	}
+
+	@objc func keyboardWillHide(_ notification: Notification) {
+		let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+		modalWindow.frame.origin.y += keyboardSize!.height / 4
+	}
+	
+}
---
+++ ViewControllers/ModalViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ModalViewController.swift
+//  Dictioly
+//
+//  Created by Nikolay Goncharenko on 14.04.2021.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+	
+	@IBOutlet weak var modalWindow: UIView!
+	@IBOutlet weak var modalHeadingLabel: UILabel!
+	@IBOutlet weak var modalTextField: UITextField!
+	@IBOutlet weak var modalCreateButton: UIButton!
+	@IBOutlet weak var modalCloseButton: UIButton!
+	
+//	@IBOutlet weak var flagFromLeftSide: UIImageView!
+//	@IBOutlet weak var flagFromRightSide: UIImageView!
+//	@IBOutlet weak var languageLabelFromLeftSide: UILabel!
+//	@IBOutlet weak var languageRightFromLeftSide: UILabel!
+	
+	@IBOutlet weak var flagFromLeftSide: UIButton!
+	@IBOutlet weak var flagFromRightSide: UIButton!
+	@IBOutlet weak var languageButtonFromLeftSide: UIButton!
+	@IBOutlet weak var languageButtonFromRightSide: UIButton!
+	
+	@IBOutlet weak var reverseButton: UIButton!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//		view.isOpaque = false
+//		view.backgroundColor = UIColor.gray.withAlphaComponent(0.4)
+//		modalWindow.backgroundColor = .white
+//		view.backgroundColor = UIBlurEffect(style: .prominent)
+//		view.layer.backgroundColor = CGColor(gray: 1, alpha: 0.4)
+		modalWindow.layer.cornerRadius = 8
+		modalTextField.layer.cornerRadius = 8
+		modalCreateButton.layer.cornerRadius = 8
+		modalCloseButton.layer.cornerRadius = 8
+		
+		modalTextField.delegate = self
+		modalTextField.becomeFirstResponder()
+		modalTextField.clearButtonMode = .whileEditing
+		modalTextField.layer.borderWidth = 0.1
+		modalTextField.layer.cornerRadius = 8
+//		modalTextField.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+//		modalTextField.tintColor = #colorLiteral(red: 0.8235294118, green: 0.8235294118, blue: 0.8274509804, alpha: 1)
+		modalTextField.layer.masksToBounds = true
+		
+		flagFromLeftSide.layer.borderWidth = 0.1
+		flagFromLeftSide.layer.cornerRadius = 12.5
+		flagFromLeftSide.layer.borderColor = #colorLiteral(red: 0.8235294118, green: 0.8235294118, blue: 0.8274509804, alpha: 1)
+		flagFromLeftSide.layer.masksToBounds = true
+		
+		flagFromRightSide.layer.borderWidth = 0.1
+		flagFromRightSide.layer.cornerRadius = 12.5
+		flagFromRightSide.layer.borderColor = #colorLiteral(red: 0.8235294118, green: 0.8235294118, blue: 0.8274509804, alpha: 1)
+		flagFromRightSide.layer.masksToBounds = true
+		
+		modalCreateButton.isEnabled = false
+		modalCreateButton.backgroundColor = #colorLiteral(red: 0.9490322471, green: 0.9485961795, blue: 0.9704589248, alpha: 1)
+		modalCreateButton.setTitleColor(#colorLiteral(red: 1, green: 0.2705882353, blue: 0.2274509804, alpha: 1), for: .disabled)
+		modalTextField.addTarget(self, action: #selector(blockingModalCreateButton), for: .editingChanged)
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+	
+	// MARK: - Actions of ModalViewContoller
+	
+	@IBAction func createNewElement(_ sender: Any) {
+		if modalTextField.text?.isEmpty == false {
+			addItem(nameItem: modalTextField.text!)
+		}
+		dismiss(animated: true, completion: nil)
+	}
+	
+	@objc func blockingModalCreateButton() {
+		if modalTextField.text!.isEmpty == true {
+			modalCreateButton.isEnabled = false
+			modalCreateButton.backgroundColor = #colorLiteral(red: 0.9490322471, green: 0.9485961795, blue: 0.9704589248, alpha: 1)
+			modalCreateButton.setTitleColor(#colorLiteral(red: 1, green: 0.2705882353, blue: 0.2274509804, alpha: 1), for: .disabled)
+		} else {
+			modalCreateButton.isEnabled = true
+			modalCreateButton.backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.8431372549, blue: 0.2941176471, alpha: 1)
+			modalCreateButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+		}
+	}
+	
+	@IBAction func closeCurrentView(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+}
+
+extension ModalViewController: UITextFieldDelegate {
+
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		modalTextField.resignFirstResponder()
+	}
+
+	@objc func keyboardWillShow(_ notification: Notification) {
+		let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+		if modalWindow.frame.maxY != keyboardSize!.origin.y {
+			modalWindow.frame.origin.y -= keyboardSize!.height / 4
+		}
+	}
+
+	@objc func keyboardWillHide(_ notification: Notification) {
+		let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+		modalWindow.frame.origin.y += keyboardSize!.height / 4
+	}
+	
+}
